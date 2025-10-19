47d4fd9bd0ac04f10b312aec4fbfb8da2e8db828
i3vie <i3vie@i3vie.dev>
2025-10-19 14:20:08 -0500

    limine files included

---
+++ src/drivers/limine/limine_requests.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "./limine.h"
+
+__attribute__((used, section(".limine_requestss")))
+static volatile LIMINE_BASE_REVISION(3);
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile LIMINE_REQUESTS_END_MARKER;
