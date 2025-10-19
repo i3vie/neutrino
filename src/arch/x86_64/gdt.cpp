99da96ae64a6ced12ac0c8ac69312eaa90a60eba
i3vie <i3vie@i3vie.dev>
2025-10-19 15:29:32 -0500

    add usermode GDT entries

--- src/arch/x86_64/gdt.cpp
+++ src/arch/x86_64/gdt.cpp
@@ -52,11 +52,15 @@ void gdt_install() {
     } __attribute__((packed)) gdtr;
 
     memset(gdt_area, 0, sizeof(gdt_area));
-    set_gdt_entry_bytes(gdt_area + 1*8, 0, 0, 0x9A, 0x20);
-    set_gdt_entry_bytes(gdt_area + 2*8, 0, 0, 0x92, 0x00);
-    set_tss_descriptor_bytes(gdt_area + 6*8, (uint64_t)&tss, sizeof(TSS)-1, 0x89, 0x00);
+    set_gdt_entry_bytes(gdt_area + 0*8, 0, 0, 0, 0); // null
+    set_gdt_entry_bytes(gdt_area + 1*8, 0, 0x000FFFFF, 0x9A, 0x20); // kernel code (L=1)
+    set_gdt_entry_bytes(gdt_area + 2*8, 0, 0x000FFFFF, 0x92, 0x00); // kernel data
+    set_gdt_entry_bytes(gdt_area + 3*8, 0, 0x000FFFFF, 0xFA, 0x20); // user code DPL=3 L=1
+    set_gdt_entry_bytes(gdt_area + 4*8, 0, 0x000FFFFF, 0xF2, 0x00); // user data DPL=3
 
-    gdtr.limit = sizeof(gdt_area) - 1;
+    set_tss_descriptor_bytes(gdt_area + 6*8, (uint64_t)&tss - KERNEL_VIRT_BASE, sizeof(TSS)-1, 0x89, 0x00); // TSS at index 5
+   
+    gdtr.limit = (uint16_t)((8*8) - 1);
     gdtr.base  = (uint64_t)&gdt_area;
 
     load_gdt_ptr(&gdtr);
