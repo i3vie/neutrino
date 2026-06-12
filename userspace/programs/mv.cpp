66e6b161f549f1643019f64e0944d1620bdfa530
i3vie <i3vie@i3vie.dev>
2026-06-11 23:37:31 -0500

    Add mv command

---
+++ userspace/programs/mv.cpp
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <neutrino.h>
+#include "../crt/syscall.hpp"
+
+int main(uint64_t arg_ptr, uint64_t) {
+    const char* args = reinterpret_cast<const char*>(arg_ptr);
+
+    long console = neutrino_open_stdout();
+
+    char source[128];
+    char dest[128];
+    if (!neutrino_parse_two_args(args, source, sizeof(source), dest, sizeof(dest))) {
+        neutrino_write_line(console, "usage: mv <source> <dest>");
+        return 1;
+    }
+
+    if (!neutrino_copy_file(source, dest)) {
+        neutrino_write(console, "mv: unable to copy ");
+        neutrino_write(console, source);
+        neutrino_write(console, " to ");
+        neutrino_write_line(console, dest);
+        return 1;
+    }
+    if (file_remove(source) < 0) {
+        file_remove(dest);
+        neutrino_write(console, "mv: unable to remove ");
+        neutrino_write_line(console, source);
+        return 1;
+    }
+    return 0;
+}
